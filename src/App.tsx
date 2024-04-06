8e09714f59da299a2710b1f6bdcce65529a21cee
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2024-04-05 22:55:51 -0400

    Applied use effect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import './App.css'
 import { TransactionType } from './types/TransactionType'
 import { AddTransaction } from './components/addTransaction'
@@ -7,28 +7,31 @@ import { TransactionElement } from './components/TransactionElement'
 export const TransactionContext = createContext(0);
 
 function App() {
-  const [balance, setBalance] = useState<number>(0)
   const [income, setIncome] = useState<number>(0)
   const [expense, setExpense] = useState<number>(0)
   const [transactions, setTransactions] = useState<TransactionType[]>([])
 
-  const handleBalanceChange = (transaction: TransactionType) => {
-    const amount = parseFloat(transaction.amount)
-    const amountSign = transaction.amount[0]
+  useEffect(() => {
+    let localIncome = 0
+    let localExpense = 0
 
-    setBalance(balance + amount)
+    transactions.forEach((transaction) => {
+      const amount = parseFloat(transaction.amount)
 
-    if (amountSign === "-") {
-      setExpense(amount)
-    } else {
-      setIncome(amount)
-    }
-  }
+      if (amount > 0) {
+        localIncome += amount
+      } else {
+        localExpense += amount
+      }
+
+    })
+    setIncome(localIncome)
+    setExpense(localExpense)
+  }, [transactions])
 
   const onAdd = (transaction: TransactionType) => {
-    console.log(transaction.id)
+    alert(income)
     setTransactions([...transactions, transaction])
-    handleBalanceChange(transaction)
   }
 
   const onDelete = (id: number) => {
@@ -42,7 +45,7 @@ function App() {
           <h1 className='font-bold text-4xl text-center mb-6'>Expense Tracker</h1>
           <div className="flex flex-col">
             <p>Your balance</p>
-            <p>${balance}</p>
+            <p>${income + expense}</p>
           </div>
           <div className="flex flex-row bg-white w-full gap-8 text-center justify-center p-4">
             <div className="flex flex-col">
